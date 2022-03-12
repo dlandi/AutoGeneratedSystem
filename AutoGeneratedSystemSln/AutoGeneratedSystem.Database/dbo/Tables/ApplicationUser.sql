ef1aecc04da9a96f09d611efd93cb6efacb01bee
Eduardo Fonseca <eduardo.fonseca@pticostarica.com>
2022-03-12 14:31:42 -0600

    Adding database and services projects

---
+++ AutoGeneratedSystemSln/AutoGeneratedSystem.Database/dbo/Tables/ApplicationUser.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ApplicationUser] (
+    [ApplicationUserId] BIGINT             NOT NULL IDENTITY,
+    [Name]              NVARCHAR (50)      NOT NULL,
+    [Lastname]          NVARCHAR (50)      NOT NULL,
+    [Email]             NVARCHAR (50)      NOT NULL,
+    [BirthDate]         DATETIMEOFFSET (7) NOT NULL,
+    CONSTRAINT [PK_ApplicationUser] PRIMARY KEY CLUSTERED ([ApplicationUserId] ASC)
+);
+
