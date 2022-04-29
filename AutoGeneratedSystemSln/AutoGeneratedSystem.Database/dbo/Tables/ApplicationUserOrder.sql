1a72c90fb134555263486bc556d511c52f14609a
Eduardo Fonseca <eduardo.fonseca@pticostarica.com>
2022-04-29 06:33:13 -0600

    Addiing the order table

---
+++ AutoGeneratedSystemSln/AutoGeneratedSystem.Database/dbo/Tables/ApplicationUserOrder.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ApplicationUserOrder]
+(
+	[ApplicationuserOrderId] BIGINT NOT NULL CONSTRAINT PK_ApplicationUserOrder PRIMARY KEY IDENTITY, 
+    [ApplicationUserId] BIGINT NOT NULL, 
+    [OrderTotal] MONEY NOT NULL, 
+    [OrderDate] DATETIMEOFFSET NOT NULL DEFAULT GetUtcDate(), 
+    CONSTRAINT [FK_ApplicationUserOrder_ApplicationUser] FOREIGN KEY ([ApplicationUserId]) REFERENCES [ApplicationUser]([ApplicationUserId])
+)
